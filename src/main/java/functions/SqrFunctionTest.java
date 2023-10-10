2928b30592ac6e3fbf9bbf5b7264205229455086
Yalukhov <a.z10293841@gmail.com>
2023-10-11 00:19:12 +0400

    LR2

---
+++ src/main/java/functions/SqrFunctionTest.java
@@ -0,0 +1,19 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SqrFunctionTest {
+
+    @Test
+    void testApply() {
+        MathFunction sqrFunction = new SqrFunction();
+
+        assertEquals(0.0, sqrFunction.apply(0.0), 0.0001);
+        assertEquals(1.0, sqrFunction.apply(1.0), 0.0001);
+        assertEquals(4.0, sqrFunction.apply(2.0), 0.0001);
+        assertEquals(25.0, sqrFunction.apply(5.0), 0.0001);
+        // Добавьте другие тестовые случаи по необходимости
+    }
+}
